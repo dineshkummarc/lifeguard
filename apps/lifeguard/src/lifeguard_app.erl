82606dba6f3f199a53a8437e50e2723bc5c740b7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-06-04 00:41:16 +0200

    LIFEGUARD_LOG to set console debug level
    Environmental variables are much more convenient for quickly setting a
    debug log level than modifying a configuraiton file. Support that.

--- apps/lifeguard/src/lifeguard_app.erl
+++ apps/lifeguard/src/lifeguard_app.erl
@@ -10,6 +10,15 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    % If we have a LIFEGUARD_LOG environmental variable, then set the console
+    % log level to whatever that is set to.
+    case os:getenv("LIFEGUARD_LOG") of
+        false -> ok;
+        Value ->
+            lager:set_loglevel(lager_console_backend, list_to_atom(Value))
+    end,
+
+    % Start the main supervisor, which kicks off the entire application process
     lifeguard_sup:start_link().
 
 stop(_State) ->
