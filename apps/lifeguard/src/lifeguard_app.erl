2571ad659bb67c995054326e90cdedff1709ec59
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-06-07 14:56:19 -0700

    Enable plugins at load time.
    Plugins are set in the list "plugins" for application configuration.

--- apps/lifeguard/src/lifeguard_app.erl
+++ apps/lifeguard/src/lifeguard_app.erl
@@ -18,8 +18,27 @@ start(_StartType, _StartArgs) ->
             lager:set_loglevel(lager_console_backend, list_to_atom(Value))
     end,
 
+    % Get the plugins we need to enable after starting the entire
+    % supervision tree.
+    {ok, Plugins} = application:get_env(plugins),
+
     % Start the main supervisor, which kicks off the entire application process
-    lifeguard_sup:start_link().
+    case lifeguard_sup:start_link() of
+        {ok, _Pid} = GoodResult ->
+            % The supervisor started up properly, so lets start up all
+            % the plugins.
+            lager:info("Enabling boot-up plugins: ~p", [Plugins]),
+            lists:foreach(fun(Name) ->
+                        % XXX: Error handling
+                        lager:debug("Enabling boot-up plugin: ~p", [Name]),
+                        lifeguard_plugin_manager:enable_plugin(Name)
+                end, Plugins),
+
+            % And return the good result
+            GoodResult;
+        Other ->
+            Other
+    end.
 
 stop(_State) ->
     ok.
