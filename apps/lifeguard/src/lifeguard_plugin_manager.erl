c5ad3daeacf16abbdae2d703ee8f5d741fbe4cc4
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-06-07 11:19:19 -0700

    plugin manager can load plugins with enable_plugin
    This currently will find the plugins in the configured plugin directory
    and then add all the ebins to the code path, finally starting the
    application.
    
    In the future, there needs to be some more error checking here because
    application:start doesn't start dependent applications and there will
    undoubtedly be some.

--- apps/lifeguard/src/lifeguard_plugin_manager.erl
+++ apps/lifeguard/src/lifeguard_plugin_manager.erl
@@ -1,6 +1,7 @@
 -module(lifeguard_plugin_manager).
 -behavior(gen_server).
--export([start_link/1]).
+-export([start_link/1,
+         enable_plugin/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {
@@ -11,6 +12,11 @@
 start_link(PluginPath) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [PluginPath], []).
 
+%% @doc Load a plugin, which is just an OTP application, with
+%% the given name.
+enable_plugin(Plugin) ->
+    gen_server:call(?MODULE, {enable_plugin, Plugin}).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% gen_server callbacks
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -19,7 +25,30 @@ init([PluginPath]) ->
     lager:info("Plugin manager started."),
     {ok, #state{plugin_path=PluginPath}}.
 
-handle_call(_Request, _From, State) -> {noreply, State}.
+handle_call({enable_plugin, Plugin}, _From, State) ->
+    PluginPath = State#state.plugin_path,
+    PluginAtom = atom_to_binary(Plugin, utf8),
+    RootPath   = <<PluginPath/binary, "/", PluginAtom/binary>>,
+
+    Result = case filelib:is_dir(RootPath) of
+        false -> {error, plugin_not_found};
+        true ->
+            % It exists, now we traverse all the lib paths and add it
+            % to the code paths. First, find all the libraries.
+            LibPath = binary_to_list(<<RootPath/binary, "/lib/*/ebin">>),
+            LibDirs = lists:filter(fun(X) -> filelib:is_dir(X) end, filelib:wildcard(LibPath)),
+
+            % Add every directory to the code path
+            lists:foreach(fun(Path) ->
+                        lager:debug("Add to code path: ~p", [Path]),
+                        true = code:add_path(Path)
+                end, LibDirs),
+
+            % Finally, start the application
+            application:start(Plugin)
+    end,
+
+    {reply, Result, State}.
 
 handle_cast(_Request, State) -> {noreply, State}.
 
